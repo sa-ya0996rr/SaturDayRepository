0e2117319c8b2e5e80673944df093a1661d1e4c6
sa-ya0996rr <s28206407@gmail.com>
2026-07-11 12:39:56 +0900

    初期化

---
+++ 20260711_中原彩月/Animation.h
@@ -0,0 +1,29 @@
+#pragma once
+class Animation
+{
+private:
+	// 分割画像
+	int* images;
+
+	// 開始フレーム
+	int startFrome;
+
+	// 
+	int frameCount;
+
+	// 
+	int speed;
+
+	// 
+	int starTime;
+private:
+	// 
+	// 
+	// 
+	Animation();
+	// 
+	void SetImages(int* img);
+	// 
+	void
+};
+
---
+++ 20260711_中原彩月/Game.cpp
@@ -0,0 +1,11 @@
+#include "Game.h"
+#include"Config.h"
+#include"Dxlib.h"
+
+void Game::Init()
+{
+	// 
+	// 
+	// 
+	LoadDivGraph("img/AnimationPlayer1.png")
+}
